f6e23a4daaea70078c774d68387ae46f1741639c
anshika-jain-coder <73584808+anshika-jain-coder@users.noreply.github.com>
2021-09-25 15:17:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+img="";
+status="";
+objects=[];
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+   
+}
+function draw(){
+    image(video,0,0,480,380)
+}
+function start(){
+    objectDetector =ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("model loaded by anshika jain");
+    status=true;
+}
